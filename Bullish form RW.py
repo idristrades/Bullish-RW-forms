c6656f640fe1e655a64c6e403f2d48a04f28d1b2
Prince Idris <gualdjouma@aim.com>
2022-07-14 17:40:33 -0400

    Update Bullish form RW.py

--- Bullish form RW.py
+++ Bullish form RW.py
@@ -1,22 +1,29 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
+#update the date so you get the most recent RW forms
+
+#############################
+date = '(since:2022-07-01)'
+#############################
 
 
-date = '(since:2022-07-12)'
 query = "files form RW" + date
 
 tweets = []
-limit = 7
+
+# to get more than the most recent 3 forms, change the number below
+limit = 3
+
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
 
     if len(tweets) == limit:
         break
     else:
-        tweets.append([tweet.date,tweet.user.username, tweet.content, tweet.tcooutlinks])
+        tweets.append([tweet.date, tweet.content[0:5], tweet.tcooutlinks])
         
-df = pd.DataFrame(tweets, columns=['Date', 'Username', 'Tweet', 'Url'])
+df = pd.DataFrame(tweets, columns=['Date', 'Ticker Symbol', 'Link to RW form'])
 print(df)
 
 #to save the data, use df.to_csv('tweets.csv')
